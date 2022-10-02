bca7e131c670be55a6a2a418822aa889c50011b5
Bagirka <bruna07@mail.ru>
2022-10-02 22:02:24 +0500

    Task_01

---
+++ Sem/Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿void Zadacha1()
+{
+int a = 3;
+int b = 5;
+int result = 1;
+
+for(int i = 1; i <= b; i++)
+{
+   result *= a;
+}
+Console.WriteLine($"число {a} в степени {b} равно {result}");
+}
+
+Zadacha1();
